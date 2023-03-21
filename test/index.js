692c36fe217f94a11796c2e0f9be8de72dad432f
zhikangfan <zhikangfan@outlook.com>
2023-03-21 15:11:38 +0800

    feat: add test

---
+++ test/index.js
@@ -0,0 +1,25 @@
+/*
+* 输出打印结果
+* */
+var name = 'global';
+var obj = {
+    name: 'local',
+    foo: function(){
+        this.name = 'foo';
+    }.bind(window)
+};
+var bar = new obj.foo();
+setTimeout(function() {
+    console.log(window.name);
+}, 0);
+console.log(bar.name);
+
+var bar3 = bar2 = bar;
+bar2.name = 'foo2';
+console.log(bar3.name);
+
+/*
+* foo
+* foo2
+* global
+* */
---
+++ test/yield.js
@@ -0,0 +1,15 @@
+// async function t(){
+//     return 1;
+// }
+//
+// function* get() {
+//     yield 1;
+//     yield 2
+// }
+// let a = get();
+// console.log(a.next()) //{ value: 1, done: false }
+// console.log(a.next()) //{ value: 2, done: false }
+// console.log(a.next()) //{ value: undefined, done: true }
+
+
+
